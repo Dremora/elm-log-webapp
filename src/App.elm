325bdb0c1401f61e8f5a0944d0eb9572e91abfd4
Kirill Korolyov <kirill.korolyov@gmail.com>
2017-06-12 19:30:51 +0100

    Add menu

--- src/App.elm
+++ src/App.elm
@@ -1,10 +1,13 @@
 module App exposing (..)
 
-import Html exposing (Html, text, div, img)
-import Html.Attributes exposing (src)
-
-
----- MODEL ----
+import Color
+import Element exposing (..)
+import Element.Attributes exposing (..)
+import Html exposing (Html)
+import Style exposing (..)
+import Style.Color as StyleColor
+import Style.Border as Border
+import Style.Font as Font
 
 
 type alias Model =
@@ -17,10 +20,6 @@ init () =
     ( { message = "Your Elm App is working!" }, Cmd.none )
 
 
-
----- UPDATE ----
-
-
 type Msg
     = NoOp
 
@@ -30,17 +29,59 @@ update msg model =
     ( model, Cmd.none )
 
 
+type Styles
+    = None
+    | Navigation
+    | MenuHeading
+    | MenuItem
 
----- VIEW ----
+
+stylesheet : StyleSheet Styles variation
+stylesheet =
+    Style.stylesheet
+        [ style None []
+        , style Navigation
+            [ StyleColor.background (Color.rgb 25 24 24) ]
+        , style MenuHeading
+            [ StyleColor.background (Color.rgb 31 141 214)
+            , StyleColor.text (Color.rgb 255 255 255)
+            , Font.size 18
+            , Font.uppercase
+            ]
+        , style MenuItem
+            [ StyleColor.text (Color.rgb 153 153 153)
+            , StyleColor.border (Color.rgb 51 51 51)
+            , Border.bottom 1
+            , Border.solid
+            , hover
+                [ StyleColor.background (Color.rgb 51 51 51)
+                , cursor "pointer"
+                ]
+            ]
+        ]
+
+
+menuHeading =
+    el MenuHeading [ paddingXY 10 11 ] (text "Community")
+
+
+navigation =
+    column Navigation
+        [ width (px 150) ]
+        [ menuHeading
+        , el MenuItem [ paddingXY 10 11 ] (text "Events")
+        , el MenuItem [ paddingXY 10 11 ] (text "Meetups")
+        ]
+
+
+contents =
+    el None [ width (fill 1) ] (text "Contents")
 
 
 view : Model -> Html Msg
 view model =
-    div [] []
-
-
-
----- PROGRAM ----
+    Element.root stylesheet <|
+        row None [ height (percent 100), width (percent 100) ] [ navigation, contents ]
 
 
 main : Program () Model Msg
